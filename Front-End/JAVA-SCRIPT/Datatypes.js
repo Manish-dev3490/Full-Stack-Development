63e37c0fc5f8dc5ccf82e362311879008ca9aa55
manish-dev3490 <kmanish87064@gmail.com>
2025-01-16 19:49:56 +0530

    Datatypes conversion knowledge

---
+++ Front-End/JAVA-SCRIPT/Datatypes.js
@@ -0,0 +1,50 @@
+// Datatypes= So if we want to store the data informartion of the user it can be in diffrent type like number, text . so in javascript there are two types of datatypes are there in the following 
+
+// Primitive datatypes= string , number , undefined , null , bigint, symbol ,boolean. we have seen all these datatypes in variable.js files now i am going to show you the datatype conversion.
+
+// Number into string
+let myAge=22;
+myAge=String(myAge);
+console.log(myAge);
+console.log(typeof myAge);
+
+
+// /String into Number
+let myName="50abc";
+myName=Number(myName);
+console.log(typeof myName);
+console.log(myName);
+
+
+// Number to Boolean
+let decision=1;
+decision=Boolean(decision);
+console.log(typeof decision);
+console.log(decision);
+
+
+
+// string to boolean
+let text="manish";
+text=Boolean(text);
+console.log(typeof text);
+console.log(text);
+
+// Null to boolean
+let check=null;
+check=Boolean(check);
+console.log(typeof check);
+console.log(check);
+
+
+// undefined to boolean
+let checkTwo=undefined;
+checkTwo=Boolean(checkTwo);
+console.log(typeof checkTwo);
+console.log(checkTwo);
+
+
+
+
+
+
