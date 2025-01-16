87d9ea77f6329604f9133e7087b66baee4185258
manish-dev3490 <kmanish87064@gmail.com>
2025-01-16 20:42:29 +0530

    type coercion

---
+++ Front-End/JAVA-SCRIPT/Conversion.js
@@ -0,0 +1,22 @@
+//  Conversion = conversion is a implicitly pperform operations by the javascript engine let us see in the code file .it only based on operators if operator is +.
+
+
+// number + string = if either of operands is string then all operands will convert into string and string concatination will perform by engine
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+"2"+5+4+3);
+console.log(5+4+3+"1"+"2");
+
+
+
+// number - string = if the operator is - the subtraction will be performed on every operations
+console.log(1-"2");
+console.log("1"-"2");
+console.log("10"-"2");
+console.log("manish"-10);
+
+
+
+
+
+
