bdbb1c17e8dd33e1927c41de750c9f72663adf33
manish-dev3490 <kmanish87064@gmail.com>
2025-01-24 18:46:19 +0530

    understoofd the conspiracy of addresses

---
+++ Front-End/JAVA-SCRIPT/Arrays.js
@@ -0,0 +1,13 @@
+// In this file we are going to learn about variation of object - Array - Array is a data structure which is used to store the elements in the contigous location .
+
+let arr=['manish','kumar',undefined,null,true,false];
+
+
+let user ={
+    fname:"manish",
+    lname:"kumar"
+}
+
+let user2=user;
+user2.age=22;
+user2="manish kumar the great";
